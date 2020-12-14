d87f651fe6f3d5694b24046493ef76a40d927f78
Sprocket06 <24927632+Sprocket06@users.noreply.github.com>
2020-12-14 16:41:54 -0500

    Organization
    Separate things out in to folders so things feel less cluttered.

---
+++ Main.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class Main : Node2D
+{
+	PackedScene Player = (PackedScene)ResourceLoader.Load("res://Player.tscn");
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		AddChild(Player.Instance());
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+			
+	}
+}
